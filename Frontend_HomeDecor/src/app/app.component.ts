f865e4b3293b3898193c00963689b6f0f50db104
Giran <giran5695pro99@gmail.com>
2025-01-12 10:22:36 +0530

    nav Modification changes

--- Frontend_HomeDecor/src/app/app.component.ts
+++ Frontend_HomeDecor/src/app/app.component.ts
@@ -1,20 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { ApiService } from './api.service';
 import { CartService } from './cart.service';
 import { AuthService } from './auth.service';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, FormsModule, CommonModule, UserProfileComponent],
+  imports: [RouterOutlet, RouterLink, FormsModule, CommonModule, UserProfileComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
+
+  iscontact_Us: boolean = false
+  iscontent: boolean = false
   title = 'Frontend_HomeDecor';
   cartCount = 0;
   searchText: any = '';
@@ -22,7 +26,7 @@ export class AppComponent implements OnInit{
 
  demodecor:string = "https://i.pinimg.com/originals/96/21/95/962195497555a7631ecbddc55224884e.gif"
 
-  constructor(private apiService: ApiService, private cartService: CartService, private authService: AuthService){}
+  constructor(private apiService: ApiService, private cartService: CartService, private authService: AuthService, private router :Router){}
 
   ngOnInit(): void {
       this.cartService.currentItems.subscribe((data) => {
@@ -43,5 +47,38 @@ export class AppComponent implements OnInit{
     this.search()
   }
 
+
+  
+  scrollToSection() {
+    // Navigate to the home page
+    this.router.navigate(['/']).then(() => {
+      // Show the section
+      this.iscontent = true;
+
+      // Optionally, scroll to the element
+      const element = document.getElementById('content_dc');
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  }
+
+  scrollTocontact_Us(){
+    this.router.navigate(['/']).then(() => {
+      this.iscontact_Us = true;
+
+      const element2 = document.getElementById('contact_Us');
+      if(element2){
+        element2.scrollIntoView({ behavior: 'smooth'})
+      }
+    })
+  }
+
+  onScrollHome(){
+    const homeCannel = document.getElementById('homeCannel');
+    if(homeCannel){
+      homeCannel.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
   Logo:string = "https://interiorhomesdecor.com/assets/images/logo.png"
 }
